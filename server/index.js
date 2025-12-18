d7a6a635c040ea8b8fdfe0b99e17ce8901f30228
vineeth v <vineethjay1998@gmail.com>
2025-12-18 15:11:26 +0530

    Configure app for Render deployment

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ app.use('/api/events', eventRoutes);
 app.use('/api/poojas', poojaRoutes);
 app.use('/api/bookings', require('./routes/bookings'));
 
-const PORT = 3000;
+// PORT moved to bottom
 const bcrypt = require('bcryptjs');
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
@@ -43,9 +43,20 @@ async function seedAdmin() {
     }
 }
 
-app.get('/', (req, res) => {
-    res.send('Temple Application API');
+// ... imports same
+const path = require('path');
+
+// ... routes same
+
+// Serve static files from React app
+app.use(express.static(path.join(__dirname, '../client/dist')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
 });
+
+const PORT = process.env.PORT || 3000;
+// ... prisma/seedAdmin same
 
 // Start Server
 app.listen(PORT, async () => {
